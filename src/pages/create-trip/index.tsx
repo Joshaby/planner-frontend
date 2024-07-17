86b4b0f26a9f4cc9e115066f358d8ebf9d9ae3e2
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-07-16 23:49:54 -0300

    Transforma modal de convidados em componente

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,6 +1,7 @@
 import { ArrowRight, AtSign, Calendar, MapPin, Plus, Settings2, User, UserRoundPlus, X } from 'lucide-react';
 import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { InviteGuestsModal } from './invite-guests-modal';
 
 export const CreateTripPage = () => {
   const navigate = useNavigate();
@@ -179,46 +180,12 @@ export const CreateTripPage = () => {
       )}
 
       {isGuestesModalOpen && (
-        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <h2 className="text-lg font-semibold">Selecionar convidados</h2>
-                <button onClick={closeGuestsModal}>
-                  <X className="size-5 text-zinc-400" />
-                </button>
-              </div>
-              <p className="text-sm text-zinc-400">Os convidados irão receber e-mails para confirmar a participação na viagem.</p>
-            </div>
-            <div className="flex flex-wrap gap-2">
-              {emailsToInvite.map((email, idx) => (
-                <div key={idx} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
-                  <span className="text-zinc-300">{email}</span>
-                  <button>
-                    <X className="size-4 text-zinc-400" onClick={() => removeEmailFromInvite(email)} />
-                  </button>
-                </div>
-              ))}
-            </div>
-            <div className="w-full h-px bg-zinc-800"></div>
-            <form onSubmit={addEmailToInvite} className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-              <div className="px-2 flex items-center flex-1 gap-2">
-                <AtSign className="text-zinc-400 size-5" />
-                <input
-                  type="email"
-                  name="email"
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                  placeholder="Digite o e-mail do convidado"
-                />
-              </div>
-
-              <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 flex items-center gap-2 hover:bg-lime-400">
-                Convidar
-                <Plus className="size-5" />
-              </button>
-            </form>
-          </div>
-        </div>
+        <InviteGuestsModal
+          closeGuestsModal={closeGuestsModal}
+          emailsToInvite={emailsToInvite}
+          addEmailToInvite={addEmailToInvite}
+          removeEmailFromInvite={removeEmailFromInvite}
+        />
       )}
     </div>
   );
