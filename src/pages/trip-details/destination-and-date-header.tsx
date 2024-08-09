76efef53f2be27be6d2f154483782a5b51e6a87f
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-08-08 23:46:10 -0300

    Adiciona requisição ao entrar na página de trips. Após a requisição algumas informações são exibidas na tela

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -1,17 +1,39 @@
 import { Calendar, MapPin, Settings2 } from 'lucide-react';
 import { Button } from '../../components/button';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { api } from '../../lib/axios';
+import { format } from 'date-fns';
+
+interface Trip {
+  id: string;
+  destination: string;
+  starts_at: string;
+  ends_at: string;
+  is_confirmed: boolean;
+}
 
 export const DestinationAndDateHeader = () => {
+  const { tripId } = useParams();
+  const [trip, setTrip] = useState<Trip | undefined>();
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}`).then((response) => setTrip(response.data.trip));
+  }, [tripId]);
+
+  const displayedDate =
+    trip && trip.starts_at && trip.ends_at ? format(trip.starts_at, "d 'de' LLL").concat(' até ').concat(format(trip.ends_at, "d 'de' LLL")) : null;
+
   return (
     <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
       <span className="flex items-center gap-2">
         <MapPin className="size-5 text-zinc-400" />
-        <span className="text-zinc-100">Florianópolis, Brasil</span>
+        <span className="text-zinc-100">{trip?.destination}</span>
       </span>
       <div className="flex items-center gap-5">
         <span className="flex items-center gap-2">
           <Calendar className="size-5 text-zinc-400" />
-          <span className="text-zinc-100">17 a 23 de Agosto</span>
+          <span className="text-zinc-100">{displayedDate}</span>
         </span>
         <div className="w-px h-6 bg-zinc-800" />
         <Button variant="secondary">
